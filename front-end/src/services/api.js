b6c77b33f215a88bb33af3286f244820b107a784
mouhamad SALL <126395117+MHD-sall@users.noreply.github.com>
2025-10-14 04:14:55 +0200

    Update api.js

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // Configuration pour Docker - utilise l'URL de l'environnement ou une valeur par défaut
 const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
-
+const ()
 console.log('API Base URL:', API_BASE_URL)
 
 const api = axios.create({
